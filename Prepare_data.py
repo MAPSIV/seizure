e35849bb66ee925128ddcfe2137eced1d2c011dc
Roberto Manresa <80832973+Roberto-Uni@users.noreply.github.com>
2022-11-29 14:03:22 +0200

    Add files via upload

--- Prepare_data.py
+++ Prepare_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sys
 import os
+import numpy as np
 
 def seizure_id(line):
-    F = 64
     S = 30
     if line["type"] == "normal":
         return 0
@@ -23,13 +23,34 @@ save_path = "Datos_procesados"
 if not os.path.exists('Datos_procesados'):
    os.makedirs('Datos_procesados')
 
-for file in os.listdir(path):
-    if file.endswith(".parquet"):
-        print("Procesando", file)
-        df_data = pd.read_parquet(os.path.join(path, file))
+if len(sys.argv)>2:
+    F = int(sys.argv[2])
+else:
+    F = 128
+Division_window = F//2
+
+for filename in os.listdir(path):
+    if filename.endswith(".parquet"):
+        print("Procesando", filename)
+        df_data = pd.read_parquet(os.path.join(path, filename))
         df_data["id"] = df_data.groupby("filename").cumcount()
         df_data["tag"] = df_data.apply(seizure_id, axis=1)
-        df_data.to_parquet(os.path.join(save_path, file))
-
-
-
+        df_data.to_parquet(os.path.join(save_path, filename))
+        windows = []
+        metadatos = []
+        for file in df_data["filename"].unique():
+            df_file = df_data[df_data["filename"] == file]
+            for tag in [0, 1, 2, 3]:
+                df_tag = df_file[df_file["tag"] == tag].drop(["type", "PatID", "filename", "id", "tag"], axis=1)
+                df_tag = df_tag.drop(df_tag.tail(df_tag.shape[0]%F).index)
+                if df_tag.shape[0]>0:
+                    windows = windows + np.array_split(df_tag, df_tag.shape[0]/F)
+                    metadatos = metadatos + [[file, tag] for _ in range(int(df_tag.shape[0]/F))]
+                    if tag == 1:
+                        for i in range(Division_window):
+                            df_tag = np.roll(df_tag, int(-F/Division_window), axis=0)
+                            windows = windows + np.array_split(df_tag, df_tag.shape[0]/F)
+                            metadatos = metadatos + [[file, tag] for _ in range(int(df_tag.shape[0]/F))]
+        np.savez(os.path.join(save_path, filename[:filename.find(".")]+"_windows"), windows = windows)
+        metadatos = pd.DataFrame(metadatos, columns=["filename", "tag"])
+        metadatos.to_parquet(os.path.join(save_path, filename[:filename.find(".")]+"_metadatos.parquet"))
